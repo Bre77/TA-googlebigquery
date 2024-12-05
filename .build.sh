2a45668aad2e0cb8da7e0b4af39adcbc4c42babc
Brett <brett.whynot@gmail.com>
2024-12-05 10:26:37 +1000

    Lock in existing dependancies

--- .build.sh
+++ .build.sh
@@ -4,8 +4,7 @@ OUTPUT="${1:-TA-googlebigquery.spl}"
 chmod -R u=rwX,go= *
 chmod -R u-x+X *
 chmod -R u=rwx,go= bin/*
-python3.9 -m pip install --upgrade --no-dependencies -t lib -r lib/requirements.txt
-rm -rf lib/*/__pycache__
-rm -rf lib/*/*/__pycache__
+python3.9 -m pip install --upgrade -t lib -r lib/requirements.txt
+find lib -type d -name "__pycache__" -exec rm -r {} +
 cd ..
 tar -cpzf $OUTPUT --exclude=.* --exclude=package.json --overwrite TA-googlebigquery
