52818746e828934edd432085755e1f6e52d8c972
Brett Adams <brett.whynot@gmail.com>
2026-04-02 20:51:07 +1000

    Back to python3.9

--- .build.sh
+++ .build.sh
@@ -4,6 +4,6 @@ OUTPUT="${1:-TA-googlebigquery.spl}"
 chmod -R u=rwX,go= *
 chmod -R u-x+X *
 chmod -R u=rwx,go= bin/*
-python3.13 -m pip install --upgrade -t lib -r lib/requirements.txt
+python3.9 -m pip install --upgrade -t lib -r lib/requirements.txt
 cd ..
 COPYFILE_DISABLE=1 tar -cpzf $OUTPUT --exclude='*/.*' --exclude=.* --exclude=package.json --exclude='__pycache__' TA-googlebigquery
